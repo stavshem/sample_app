52e02bd1913ec6ecb689c00d4da10803062c3589
Stav Shemesh <stav@Alons-MacBook-Pro.local>
2014-05-13 18:20:05 +0300

    minor changes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,71 +2,98 @@ require 'spec_helper'
 
 describe "User pages" do
 
-	subject { page }
+  subject { page }
 
-	describe "profile page" do
-		let(:user) { FactoryGirl.create(:user) }
-		before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-		it { should have_content(user.name) }
-		it { should have_selector('h1', text: user.name) }
-		it { should have_selector('title', text: user.name) }
+    it { should have_content(user.name) }
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
 
-	end
+  end
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
 
-	describe "signup" do
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
 
-		before { visit signup_path }
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
 
-		let(:submit) { "Create my account" }
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
 
-		describe "with invalid information" do
-			it "should not create a user" do
-				expect { click_button submit }.not_to change(User, :count)
-			end
-			describe "after submission" do
-				before { click_button submit }
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
 
-				it { should have_content('error') }
-			end
-		end
+  describe "signup" do
 
-		describe "with valid information" do
-			before do
-				fill_in "Name",         with: "Example User"
-				fill_in "Email",        with: "user@example.com"
-				fill_in "Password",     with: "foobar"
-				fill_in "Confirmation", with: "foobar"
-			end
+    before { visit signup_path }
 
-			it "should create a user" do
-				expect { click_button submit }.to change(User, :count).by(1)
-			end
+    let(:submit) { "Create my account" }
 
-			describe "after saving the user" do
-				before { click_button submit }
-				let(:user) { User.find_by_email('user@example.com') }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+      describe "after submission" do
+        before { click_button submit }
 
-				it { should have_selector('title', text: user.name)}
-			end
+        it { should have_content('error') }
+      end
+    end
 
-		end
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
-		#describe "edit" do
-			#let(:user) { FactoryGirl.create(:user) }
-			#before { visit edit_user_path(user) }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
 
-			#describe "page" do
-				#it { should have_content("Update your profile") }
-				#it { should have_selector('title', text: 'Edit user') }
-				#it { should have_link('change', href: 'http://gravatar.com/emails') }
-			#end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
-			#describe "with invalid information" do
-				#before { click_button "Save changes" }
+        it { should have_selector('title', text: user.name)}
+      end
 
-				#it { should have_content('error') }
-			#end
-		#end
-	end
+    end
+
+    #describe "edit" do
+    #let(:user) { FactoryGirl.create(:user) }
+    #before { visit edit_user_path(user) }
+
+    #describe "page" do
+    #it { should have_content("Update your profile") }
+    #it { should have_selector('title', text: 'Edit user') }
+    #it { should have_link('change', href: 'http://gravatar.com/emails') }
+    #end
+
+    #describe "with invalid information" do
+    #before { click_button "Save changes" }
+
+    #it { should have_content('error') }
+    #end
+    #end
+  end
 end
